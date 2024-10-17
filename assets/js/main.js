e6a7a24a2e17a99ac06fd61ec3d23b830c85ada6
Diana-Petruccetti <dianapetruccetti@gmail.com>
2024-10-17 17:50:05 +0200

    emails log successfully

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 
 /* Creo un ciclo for nel quale inserisco dalla libreria axios  */
 for (let i = 0; i < 10; i++){
-    axios.get(flynn.boolean.careers/exercises/api/random/mail).then ((response)=> {
+    axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then ((response)=> {
         console.log(response.data)
     })
 }
